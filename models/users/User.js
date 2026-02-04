bf14047254a66eb869f1dfe97722bc690912a49d
Emoji0805 <mamihery.rabiazamaholy@gmail.com>
2026-02-04 11:46:42 +0300

    Ajout API login et inscription

--- models/users/User.js
+++ models/users/User.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  mail: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
+  mail: { type: String, required: true },
   password: { type: String, required: true },
   role: {
     type: String,
     enum: ['ADMIN', 'SHOP', 'USER'],
+    default: 'USER',
     required: true
   }
 }, { timestamps: true });
---
+++ routes/auth/AuthRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const authService = require('../../services/auth/AuthService');
+
+router.post('/register', async (req, res) => {
+  try {
+    const user = await authService.register(req.body);
+    res.status(201).json({success: 'User registered successfully',user});
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const result = await authService.login(name, password);
+    res.status(201).json({success: 'Login successfully', result});
+
+  } catch (err) {
+    res.status(401).json({ error: err.message });
+  }
+});
+
+module.exports = router;
